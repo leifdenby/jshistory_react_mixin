a011daa3ffad4dcfb30977565f0d36e2f251ee08
Marko Polojärvi <marko@avatar.ai>
2014-07-20 10:42:00 +0300

    added component ids

--- components/react-mixins/JSHistory.mixin.jsx
+++ components/react-mixins/JSHistory.mixin.jsx
@@ -38,6 +38,23 @@ var HistoryJSMixin = {
       }
     }
   },
+  deserializeState: function(state) {
+      return state[this._rootNodeID]
+  },
+  serializeState: function() {
+      var st = {}
+      st[this._rootNodeID] = {}
+      if(this.saveStates && this.saveStates.length > 0){
+          for(var key in this.state){
+              if(this.saveStates.indexOf(key) !== -1) st[this._rootNodeID][key] = this.state[key]
+          }
+      } else {
+          for(var key in this.state){
+              st[this._rootNodeID][key] = this.state[key]
+          }
+      }
+      return st
+  },
   saveState: function() {
     var serialized_state = null;
     if (this.serializeState !== undefined) {
